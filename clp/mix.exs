0c89c62948ea02b8f6bca235899dc28858fa22ca
Max Groenlund <max@synthmax.dk>
2025-06-10 08:14:25 +0200

    Start clp_engine

--- clp/mix.exs
+++ clp/mix.exs
@@ -1,12 +1,12 @@
-defmodule ClpCli.MixProject do
+defmodule Clp.MixProject do
   use Mix.Project
 
   def project do
     [
-      app: :clp_cli,
+      app: :clp,
       version: "0.1.0",
       elixir: "~> 1.17",
-      escript: [main_module: ClpCli],
+      escript: [main_module: Clp],
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
--- clp_cli/test/clp_cli_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule ClpCliTest do
-  use ExUnit.Case
-  doctest ClpCli
-
-  test "greets the world" do
-    assert ClpCli.hello() == :world
-  end
-end
